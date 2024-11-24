8f3cc38f326f2968995dbb5ae66001c442a79781
Jackson Vieira <jacksonvieiira12@gmail.com>
2024-11-24 09:51:23 -0300

    corrigir merge data script

--- hackathon/scripts/merge_data.py
+++ hackathon/scripts/merge_data.py
@@ -9,19 +9,16 @@ cidade = load_data("cleaned/cidade.csv")
 cidade = rename_columns_with_prefix(cidade, "cidade_")
 
 usina_unidade_consumidora = clean_and_merge(usina, unidade_consumidora, "unidade_consumidora_id", "id", "_unidade_consumidora")
-usina_unidade_consumidora = usina.dropna(subset=["id_unidade_consumidora"])
 
-usina_endereco = clean_and_merge(usina, endereco, "id_endereco", "id", "_endereco_cidade")
-usina_endereco = usina.dropna(subset=["id_endereco_cidade"])
+usina_unidade_consumidora = usina_unidade_consumidora.dropna(subset=["id_unidade_consumidora"])
 
-usina = usina.merge(
-    cidade,
-    left_on="id_cidade",
-    right_on="cidade_id",
-    how="left",
-)
+usina_endereco = clean_and_merge(usina_unidade_consumidora, endereco, "id_endereco", "id", "_endereco_cidade")
 
-usina = usina.dropna(subset=["cidade_id"])
+usina_endereco = usina_endereco.dropna(subset=["id_endereco_cidade"])
+
+usina_cidade = clean_and_merge(usina_endereco, cidade, "id_cidade", "cidade_id", "")
+
+usina = usina_cidade.dropna(subset=["id_cidade"])
 
 columns_to_drop = ["id_unidade_consumidora", "id_endereco", "id_cidade", "cidade_id", "id_endereco_cidade", "cidade_id_estado"]
 usina = usina.drop(columns=columns_to_drop)
